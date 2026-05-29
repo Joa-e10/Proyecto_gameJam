a5483f6e674e7b88d6ccfcd125cd42eb63694738
Joa-e10 <joaquin.ferreyra.e@gmail.com>
2026-05-29 16:40:52 -0300

    Movimiento del player
    Movimiento del player y detección de la posición del mouse en pantalla.

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
 
-public class Characters : MonoBehaviour
+public abstract class Character : MonoBehaviour
 {
-    private int lives;
-    private int speed;
-    private Rigidbody2D _rb;
+    protected int lives;
+    protected int speed;
+    protected Rigidbody2D _rb;
 
     void Start()
     {
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,29 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class Player : MonoBehaviour
+public class Player : Character
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Camera _cameraMain;
     void Start()
     {
-        
+        speed = 5;
+        _cameraMain = Camera.main;
+        _rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    private void OnMove(InputValue inputValue) 
+    {
+        Vector2 move = inputValue.Get<Vector2>(); // Tomamos el valor recibido de la accion.
+        _rb.linearVelocity = move * speed;
+    }
+
     void Update()
     {
-        
+        Vector2 MouseWorldPoint = _cameraMain.ScreenToWorldPoint(Mouse.current.position.ReadValue()); 
+        Vector2 GunPoint = MouseWorldPoint - (Vector2)transform.position;
+
+
+        Debug.Log("Esta disparando en: "+GunPoint);
     }
 }
