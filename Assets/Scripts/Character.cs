cb8c118a0a2cfff5bc5a36e111f51edf4fd42003
Joa-e10 <joaquin.ferreyra.e@gmail.com>
2026-05-29 23:06:22 -0300

    Bala del player
    Funcion de la bala paralizante del player

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -5,15 +5,28 @@ public abstract class Character : MonoBehaviour
     protected int lives;
     protected int speed;
     protected Rigidbody2D _rb;
+    protected bool _alive = true;
 
     void Start()
     {
         
     }
 
-    public void die() 
+    public void takesDamage(int received) // metodo "recibir daño".
     {
         if (lives <= 0) 
+        {
+            _alive = false;
+        }
+        else
+        { 
+            lives = lives - received;
+        }
+    }
+
+    public void die() 
+    {
+        if (_alive == false)
         {
             Destroy(gameObject);
         }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,11 +5,16 @@ using UnityEngine.InputSystem;
 public class Player : Character
 {
     private Camera _cameraMain;
+    [SerializeField] private GameObject _bulletPlayer;
+    private Transform _positionGun;
+    private Vector2 _gunPoint;
+    private float _shootEnabled;
     void Start()
     {
         speed = 5;
         _cameraMain = Camera.main;
         _rb = GetComponent<Rigidbody2D>();
+        _positionGun = GameObject.Find("GunPlayer").GetComponent<Transform>();
     }
 
     private void OnMove(InputValue inputValue) 
@@ -18,12 +23,19 @@ public class Player : Character
         _rb.linearVelocity = move * speed;
     }
 
+    private void OnAttack(InputValue inputValue) 
+    {
+        GameObject generatedBullet = Instantiate(_bulletPlayer, _positionGun.position, Quaternion.identity);
+        Bullet bulletComponent = generatedBullet.GetComponent<Bullet>();
+        bulletComponent.setDirectionBullet(_gunPoint);
+    }
+
     void Update()
     {
         Vector2 MouseWorldPoint = _cameraMain.ScreenToWorldPoint(Mouse.current.position.ReadValue()); 
-        Vector2 GunPoint = MouseWorldPoint - (Vector2)transform.position;
+        _gunPoint = MouseWorldPoint - (Vector2)transform.position;
 
 
-        Debug.Log("Esta disparando en: "+GunPoint);
+        Debug.Log("Esta disparando en: "+_gunPoint);
     }
 }
