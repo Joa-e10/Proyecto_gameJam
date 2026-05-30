8b76768f503c681f2e26aea698d66b42f680279a
Joa-e10 <joaquin.ferreyra.e@gmail.com>
2026-05-30 00:12:46 -0300

    Disparo mejorado
    Disparo mejorado, falta realizar el stun

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,7 +8,8 @@ public class Player : Character
     [SerializeField] private GameObject _bulletPlayer;
     private Transform _positionGun;
     private Vector2 _gunPoint;
-    private float _shootEnabled;
+    private bool _shootEnabled = true;
+    private float _timeShooting = 1.5f;
     void Start()
     {
         speed = 5;
@@ -25,9 +26,13 @@ public class Player : Character
 
     private void OnAttack(InputValue inputValue) 
     {
-        GameObject generatedBullet = Instantiate(_bulletPlayer, _positionGun.position, Quaternion.identity);
-        Bullet bulletComponent = generatedBullet.GetComponent<Bullet>();
-        bulletComponent.setDirectionBullet(_gunPoint);
+        if (_shootEnabled == true) 
+        {
+            GameObject generatedBullet = Instantiate(_bulletPlayer, _positionGun.position, Quaternion.identity);
+            Bullet bulletComponent = generatedBullet.GetComponent<Bullet>();
+            bulletComponent.setDirectionBullet(_gunPoint);
+            _shootEnabled = false;
+        }
     }
 
     void Update()
@@ -35,7 +40,15 @@ public class Player : Character
         Vector2 MouseWorldPoint = _cameraMain.ScreenToWorldPoint(Mouse.current.position.ReadValue()); 
         _gunPoint = MouseWorldPoint - (Vector2)transform.position;
 
+        if (_shootEnabled == false) 
+        {
+            _timeShooting -= Time.deltaTime;
+            if (_timeShooting <= 0) 
+            {
+                _timeShooting = 1.5f;
+                _shootEnabled = true;
+            }
+        }
 
-        Debug.Log("Esta disparando en: "+_gunPoint);
     }
 }
